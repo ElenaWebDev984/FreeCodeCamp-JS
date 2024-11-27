8651aa1314e2b3953a2a6f05d8c26fa9be32ba23
ElenaWebDev984 <badalova.dev.984@gmail.com>
2024-11-27 04:30:53 +0100

    step 68

--- JavaScript Algorithms and Data Structures/Part 1/1.3. Basic LavaScript by Role Playing Game/script.js
+++ JavaScript Algorithms and Data Structures/Part 1/1.3. Basic LavaScript by Role Playing Game/script.js
@@ -9,7 +9,6 @@ let inventory = ["stick"];
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
-
 const text = document.querySelector("#text");
 const xpText = document.querySelector("#xpText");
 const healthText = document.querySelector("#healthText");
@@ -17,7 +16,6 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
-
 const locations = [
     {
         name: "town square",
@@ -41,19 +39,12 @@ button3.onclick = fightDragon;
 
 function update(location) {
     button1.innerText = location["button text"][0];
-    button2.innerText = "Go to cave";
-    button3.innerText = "Fight dragon";
-    button1.onclick = goStore;
-    button2.onclick = goCave;
-    button3.onclick = fightDragon;
-    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
-    button1.innerText = "Buy 10 health (10 gold)";
-    button2.innerText = "Buy weapon (30 gold)";
-    button3.innerText = "Go to town square";
-    button1.onclick = buyHealth;
-    button2.onclick = buyWeapon;
-    button3.onclick = goTown;
-    text.innerText = "You enter the store.";
+    button2.innerText = location["button text"][1];
+    button3.innerText = location["button text"][2];
+    button1.onclick = location["button functions"][0];
+    button2.onclick = location["button functions"][1];
+    button3.onclick = location["button functions"][2];
+    text.innerText = text.innerText = location.text;
 }
 
 function goTown() {
