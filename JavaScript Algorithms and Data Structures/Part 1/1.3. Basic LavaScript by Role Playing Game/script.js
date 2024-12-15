5f3fc0fe2f9ee3c6a0e4cbf67d8eeebd406308ee
ElenaWebDev984 <badalova.dev.984@gmail.com>
2024-12-15 01:02:13 +0100

    step 145

--- JavaScript Algorithms and Data Structures/Part 1/1.3. Basic LavaScript by Role Playing Game/script.js
+++ JavaScript Algorithms and Data Structures/Part 1/1.3. Basic LavaScript by Role Playing Game/script.js
@@ -63,6 +63,12 @@ const locations = [
         "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
         "button functions": [restart, restart, restart],
         text: "You die. &#x2620;"
+    },
+    {
+        name: "win",
+        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+        "button functions": [restart, restart, restart],
+        text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;"
     }
 ];
 
@@ -163,7 +169,7 @@ function goFight() {
 function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks.";
     text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
-    health -= monsters[fighting].level;
+    health -= getMonsterAttackValue(monsters[fighting].level);
     monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
     healthText.innerText = health;
     monsterHealthText.innerText = monsterHealth;
@@ -176,6 +182,10 @@ function attack() {
             defeatMonster();
         }
     }
+}
+
+function getMonsterAttackValue(level) {
+    
 }
 
 function dodge() {
@@ -192,6 +202,10 @@ function defeatMonster() {
 
 function lose() {
     update(locations[5]);
+}
+
+function winGame() {
+    update(locations[6]);
 }
 
 function restart() {
