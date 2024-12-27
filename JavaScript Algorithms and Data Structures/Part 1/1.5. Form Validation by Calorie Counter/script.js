77d7de82283811e286701811ec58707e8e46c911
ElenaWebDev984 <badalova.dev.984@gmail.com>
2024-12-28 00:44:37 +0100

    step 30

--- JavaScript Algorithms and Data Structures/Part 1/1.5. Form Validation by Calorie Counter/script.js
+++ JavaScript Algorithms and Data Structures/Part 1/1.5. Form Validation by Calorie Counter/script.js
@@ -8,4 +8,10 @@ let isError = false;
 
 function cleanInputString(str) {
     const regex = /[+-\s]/g;
+    return str.replace(regex, '');
 }
+
+function isInvalidInput(str) {
+    const regex = /e/i;
+}
+
