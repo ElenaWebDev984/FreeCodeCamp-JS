969ecfd6d2b6e44081008636255c3478bc9bf00c
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-01-09 03:51:02 +0100

    step 70

--- JavaScript Algorithms and Data Structures/Part 1/1.5. Form Validation by Calorie Counter/script.js
+++ JavaScript Algorithms and Data Structures/Part 1/1.5. Form Validation by Calorie Counter/script.js
@@ -26,6 +26,13 @@ function addEntry() {
   targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
+function calculateCalories(e) {
+  e.preventDefault();
+  isError = false;
+
+  const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type='number']");
+}
+
 function getCaloriesFromInputs(list) {
   let calories = 0;
 
@@ -38,7 +45,9 @@ function getCaloriesFromInputs(list) {
       isError = true;
       return null;
     }
+    calories += Number(currVal);
   }
+  return calories;
 }
 
   addEntryButton.addEventListener("click", addEntry);
