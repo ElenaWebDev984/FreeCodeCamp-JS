7702f148e82708755b3ec8a3699252683d8b76d8
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-01-30 01:00:26 +0100

    step 35

--- JavaScript Algorithms and Data Structures/Part 1/1.7. Basic String and Array Methods by Music Player/script.js
+++ JavaScript Algorithms and Data Structures/Part 1/1.7. Basic String and Array Methods by Music Player/script.js
@@ -86,6 +86,10 @@ let userData = {
     songCurrentTime: 0,
 };
 
+const playSong = (id) => {
+
+};
+
 const renderSongs = (array) => {
   const songsHTML = array.map((song) => {
     return `
@@ -110,8 +114,10 @@ const sortSongs = () => {
     if (a.title < b.title) {
       return -1;
     }
+    return 0;
   });
+  return userData?.songs;
 };
 
-renderSongs(userData?.songs);
+renderSongs(sortSongs());
 
