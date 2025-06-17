2e329e3aede5f95e29de38b97962c4a10c8a34ab
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-17 12:13:24 +0200

    step 10

--- JavaScript Algorithms and Data Structures/Part 2/2.4. Recursion by  a Decimal to Binary Converter/script.js
+++ JavaScript Algorithms and Data Structures/Part 2/2.4. Recursion by  a Decimal to Binary Converter/script.js
@@ -2,10 +2,15 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result =document.getElementById("result");
 
-function checkUserInput() {
+const checkUserInput = () => {
+    if (!numberInput.value) {}
     console.log(numberInput.value);
 }
 
 convertBtn.addEventListener("click", checkUserInput);
 
-numberInput.addEventListener("keydown", () => {});
+numberInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        checkUserInput();
+    }
+});
