719d8168f043a1def510e32d8f6467bcf70c2499
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-07-07 16:17:55 +0200

    step 75

--- JavaScript Algorithms and Data Structures/Part 2/2.4. Recursion by  a Decimal to Binary Converter/script.js
+++ JavaScript Algorithms and Data Structures/Part 2/2.4. Recursion by  a Decimal to Binary Converter/script.js
@@ -3,17 +3,22 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-   if (input === 0) {
-       return "";
-   } else  {
-       return decimalToBinary(Math.floor(input / 2)) + (input % 2);
-   }
-}
+    if (input === 0 || input === 1) {
+        return String(input);
+    } else {
+        return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+    }
+};
+
+const showAnimation = () => {}
 
 const checkUserInput = () => {
     if (!numberInput.value || isNaN(parseInt(numberInput.value)) || parseInt(numberInput.value) < 0) {
         alert("Please provide a decimal number greater than or equal to 0")
         return
+    }
+    if (parseInt(numberInput.value) === 5) {
+
     }
     result.textContent = decimalToBinary(parseInt(numberInput.value));
     numberInput.value = "";
