d1025855534746561320dea3257d6a30a84c92d7
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-07-30 20:40:31 +0200

    step 30

--- JavaScript Algorithms and Data Structures/Part 3/3.1. Regular Expressions by aSpam Filter/script.js
+++ JavaScript Algorithms and Data Structures/Part 3/3.1. Regular Expressions by aSpam Filter/script.js
@@ -5,9 +5,11 @@ const checkMessageButton = document.getElementById("check-message-btn");
 const helpRegex = /please help|assist me/i;
 const dollarRegex = /[0-9]+\s*(?:hundred|thousand|million|billion)?\s+dollars/i;
 
-const freeRegex = /fr[e3][e3] m[o0]n[e3]y/i;
+const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
 
-const denyList = [helpRegex, dollarRegex];
+const stockRegex = /st[o0]ck al[e3]rt/i;
+
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex];
 
 const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
 
