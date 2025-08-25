ee08880981998d4c95059b39c23434fb1419b1cd
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-08-25 21:18:34 +0200

    step 20

--- JavaScript Algorithms and Data Structures/Part 3/3.3. Advanced Array Methods by a Statistics Calculator/script.js
+++ JavaScript Algorithms and Data Structures/Part 3/3.3. Advanced Array Methods by a Statistics Calculator/script.js
@@ -1,7 +1,13 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
+const getMedian = (array) => {
+    const sorted = array.sort()
+}
+
 const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/, \s*/g);
     const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+    const mean = getMean(numbers);
+    document.querySelector('#mean').textContent = mean;
 }
