5a83f60c25faf18c0e10eaa9c36fef7e44c797ae
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-11-07 19:03:07 +0100

    fin project

--- JavaScript Algorithms and Data Structures/Part 3/3.4. Functional Programming by a Spreadsheet/script.js
+++ JavaScript Algorithms and Data Structures/Part 3/3.4. Functional Programming by a Spreadsheet/script.js
@@ -16,7 +16,8 @@ const highPrecedence = str => {
 const isEven = num => num % 2 === 0;
 const sum = nums => nums.reduce((acc, el) => acc + el, 0);
 const average = nums => sum(nums) / nums.length;
-const median = (nums) => {
+
+const median = nums => {
     const sorted = nums.slice().sort((a, b) => a - b);
     const length = sorted.length;
     const middle = length / 2 - 1;
@@ -26,17 +27,33 @@ const median = (nums) => {
 }
 
 const spreadsheetFunctions = {
+    '': arg => arg,
     sum,
     average,
     median,
+    even: nums => nums.filter(isEven),
+    someeven: nums => nums.some(isEven),
+    everyeven: nums => nums.every(isEven),
+    firsttwo: nums => nums.slice(0, 2),
+    lasttwo: nums => nums.slice(-2),
+    has2: nums => nums.includes(2),
+    increment: nums => nums.map(num => num + 1),
+    random: ([x, y]) => Math.floor(Math.random() * y + x),
+    range: nums => range(...nums),
+    nodupes: nums => [...new Set(nums).values()]
 }
 
-const applyFunction = (str) => {
+const applyFunction = str => {
     const noHigh = highPrecedence(str);
+    const infix = /([\d.]+)([+-])([\d.]+)/;
+    const str2 = infixEval(noHigh, infix);
+    const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
+    const toNumberList = args => args.split(",").map(parseFloat);
+    const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
+    return str2.replace(functionCall, (match, fn, args) => spreadsheetFunctions.hasOwnProperty(fn.toLowerCase()) ? apply(fn, args) : match);
 }
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
-
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
 
 const evalFormula = (x, cells) => {
@@ -48,13 +65,15 @@ const evalFormula = (x, cells) => {
     const rangeExpanded = x.replace(rangeRegex, (_match, char1, num1, char2, num2) => rangeFromString(num1, num2).map(addCharacters(char1)(char2)));
     const cellRegex = /[A-J][1-9][0-9]?/gi;
     const cellExpanded = rangeExpanded.replace(cellRegex, match => idToText(match.toUpperCase()));
+    const functionExpanded = applyFunction(cellExpanded);
+    return functionExpanded === x ? functionExpanded : evalFormula(functionExpanded, cells);
 }
 
 window.onload = () => {
-    const container = document.getElementById('container');
+    const container = document.getElementById("container");
     const createLabel = (name) => {
-        const label = document.createElement('div');
-        label.className = 'label';
+        const label = document.createElement("div");
+        label.className = "label";
         label.textContent = name;
         container.appendChild(label);
     }
@@ -73,10 +92,10 @@ window.onload = () => {
     })
 }
 
-const update = (event) => {
+const update = event => {
     const element = event.target;
-    const value = element.value.replace(/\s/g, '');
+    const value = element.value.replace(/\s/g, "");
     if (!value.includes(element.id) && value.startsWith('=')) {
-
+        element.value = evalFormula(value.slice(1), Array.from(document.getElementById("container").children));
     }
 }
